5510dfb0247b189b097fcd8604ce0279351598ec
Saket <adhav.saket@gmail.com>
2021-04-09 00:22:47 +0530

    adding basic projects and classes

---
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Qoutes { get; set; }
+
+        //This OnConfiguring methos is used here only for demo purpose. We shall inject this info from config file later on.
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog=SamuraiAppDAta");
+        }
+    }
+}
